1d9b647054fb2276ba602097ef5e5785c6095b4c
CodingwithJosh/ <joshua.laurence.fabi@gmail.com>
2024-03-15 19:06:30 +0800

    code updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ import Dashboard from './realtor/Dashboard'
 import Manage from './realtor/Manage-Property'
 import Inbox from './realtor/Inbox'
 import Settings from './realtor/Settings'
+import { BiMessageDetail } from "react-icons/bi"
+import { useState } from 'react'
+import Contact from './components/Contact'
+
 function App() {
+  const [isOpenContactModal, setIsOpenContactModal] = useState(false)
+  const toggleContactModal = () => {
+    setIsOpenContactModal(!isOpenContactModal)
+  }
   return (
     <div className='App'>
         <Routes>
@@ -16,7 +24,16 @@ function App() {
             <Route path="/properties" element={<Properties />} />
             <Route path="/realtor/*" element={<RealtorRoutes />} />
         </Routes>
-    </div>
+        <div className="fixed bottom-4 right-4">
+        <button
+          onClick={toggleContactModal}
+          className="flex items-center bg-sky-500 hover:bg-sky-700 text-white rounded-full py-4 px-5 shadow-lg"
+        >
+          <BiMessageDetail className="text-xl mr-2" />
+          Contact
+        </button>
+      </div>
+      <Contact isOpen={isOpenContactModal} onClose={() => setIsOpenContactModal(false)} />    </div>
   )
 }
 
