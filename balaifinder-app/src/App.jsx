b47bc41844e4c498778f50e0b2170a7a73c9aa5b
Vince - UCC <sy.vinceedward.bscs2020@gmail.com>
2024-03-18 16:41:02 +0800

    main+auth with fix

--- balaifinder-app/src/App.jsx
+++ balaifinder-app/src/App.jsx
@@ -11,8 +11,22 @@ import { BiMessageDetail } from "react-icons/bi"
 import { useState } from 'react'
 import Contact from './components/Contact'
 import Layout from './components/DashboardSidebar'
+import ManageProperty from './realtor/Manage-Property'
+import { useContext } from 'react'
+import { AuthContext } from './context/authContext'
+import Register from './pages/Register'
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const [isOpenContactModal, setIsOpenContactModal] = useState(false);
   const location = useLocation();
 
@@ -29,6 +43,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/properties" element={<Properties />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/realtor/*" element={<RealtorRoutes />} />
       </Routes>
       {!isRealtorRoute && (
@@ -63,55 +78,6 @@ function RealtorRoutes() {
   );
 }
 
-export default App
-import './App.css'
-import About from './pages/About'
-import Home from './pages/Home'
-import Properties from './pages/Properties'
-import Register from './pages/Register'
-import { Route, Routes } from 'react-router-dom'
-import Dashboard from './realtor/Dashboard'
-import ManageProperty from './realtor/Manage-Property'
-import Inbox from './realtor/Inbox'
-import Settings from './realtor/Settings'
-import { Navigate } from 'react-router-dom'
-import { useContext } from 'react'
-import { AuthContext } from './context/authContext'
-
-
-function App() {
-  const {currentUser} = useContext(AuthContext);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
-
-    return children;
-  };
-
-  return (
-    <div className='App'>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path='/register' element={<Register />} />
-            <Route path="/properties" element={<Properties />} />
-            <Route path="/realtor/*" element={<RealtorRoutes />} />
-        </Routes>
-    </div>
-  )
-}
-
-function RealtorRoutes(){
-  return(
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/manage-property" element={<ManageProperty />} />
-      <Route path="/inbox" element={<Inbox />} />
-      <Route path="/settings" element={<Settings />} />
-  </Routes>
-  )
-}
+export default App;
 
 
--- balaifinder-app/src/realtor/Dashboard.jsx
+++ balaifinder-app/src/realtor/Dashboard.jsx
@@ -1,20 +1,17 @@
+import DashboardSidebar from '../components/DashboardSidebar'
+
 export default function Dashboard(){
+    
     return(
         <section>
-            <div>
-                <h1>this is the dashboard</h1>
-            </div>
-        </section>
-    )
-}import DashboardSidebar from '../components/DashboardSidebar'
-
-
-function Dashboard() {
-  return (
-        <div className="p-4 bg-yellow-500">
+            <div className="p-4 bg-yellow-500">
             <h1 className="text-2xl font-bold">Dashboard Page</h1>
             <p>Welcome to the dashboard page!</p>
         </div>
-  )
+        </section>
+    )
 }
+
+
+
 
