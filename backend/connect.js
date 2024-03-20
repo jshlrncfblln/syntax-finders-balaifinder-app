a11fe9a9aba48564f63d2708fa4c96d797681b39
Paul <128304338+UCC-Paul@users.noreply.github.com>
2024-03-20 18:12:22 +0800

    initial

--- backend/connect.js
+++ backend/connect.js
@@ -1,8 +1,26 @@
+require("dotenv0").config();
 import mysql from "mysql";
 
+const urlDB = "mysql://root:nrckHamrmWoNPbwPxMYAqXoZxGfsfFOA@roundhouse.proxy.rlwy.net:11389/railway"
+
+
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "balaifinder_database",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
+
+
+
+
+// OLD CODE FOR LOCALHOST
+
+//import mysql from "mysql";
+
+//export const db = mysql.createConnection({
+  //host: "localhost",
+  //user: "root",
+  ////password: "",
+  //database: "balaifinder_database",
+//});
